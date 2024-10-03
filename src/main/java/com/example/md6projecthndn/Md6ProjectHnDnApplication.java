997de79ecffa10c5489bd020e1f4a0440f4f5e3c
Seitama179 <ngochd997@gmail.com>
2024-10-03 09:26:59 +0700

    create spring boot

---
+++ src/main/java/com/example/md6projecthndn/Md6ProjectHnDnApplication.java
@@ -0,0 +1,13 @@
+package com.example.md6projecthndn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Md6ProjectHnDnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Md6ProjectHnDnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/md6projecthndn/Md6ProjectHnDnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.md6projecthndn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Md6ProjectHnDnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
