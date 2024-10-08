0dfcf71eb964686efd661603cd110ed5a3d46d51
Seitama179 <ngochd997@gmail.com>
2024-10-08 09:34:04 +0700

    ttNgoc dang ky tai khoan

--- src/main/java/com/example/md6projecthndn/configuration/SecurityInit.java
+++
@@ -1,6 +0,0 @@
-package com.example.md6projecthndn.configuration;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
-}
